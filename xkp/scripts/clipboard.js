5de1032ee9593e4c2130dd6c488ca09f4fec7a68
Pete Otaqui <pete@otaqui.com>
2016-11-07 14:45:58 +0000

    Added xkp

---
+++ xkp/scripts/clipboard.js
@@ -0,0 +1,95 @@
+/*
+http://stackoverflow.com/a/30810322
+*/
+function copyTextToClipboard(text) {
+  if ( navigator.userAgent.match(/iPhone/) ) return;
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a flash,
+  // so some of these are just precautions. However in IE the element
+  // is visible whilst the popup box asking the user for permission for
+  // the web page to copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+
+  textArea.value = text;
+  document.body.appendChild(textArea);
+
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+  showNotification(text);
+}
+
+var notification, notificationTimeout;
+function showNotification(text) {
+  if ( !notification ) {
+    notification = document.createElement('div');
+    notification.classList.add('notification');
+    notificationContent = document.createElement('div');
+    notification.appendChild(notificationContent);
+  }
+  document.body.appendChild(notification);
+  setTimeout(function() {
+    notificationContent.textContent = '"' + text+'" to clipboard';
+    notification.classList.add('notification-show');
+  }, 1);
+  clearTimeout(notificationTimeout);
+  notificationTimeout = setTimeout(function() {
+    notification.classList.remove('notification-show');
+    notificationTimeout = setTimeout(function() {
+      document.body.removeChild(notification);
+    }, 1000);
+  }, 3000);
+}
+
+
+// var copyBobBtn = document.querySelector('.js-copy-bob-btn'),
+//   copyJaneBtn = document.querySelector('.js-copy-jane-btn');
+//
+// copyBobBtn.addEventListener('click', function(event) {
+//   copyTextToClipboard('Bob');
+// });
+//
+//
+// copyJaneBtn.addEventListener('click', function(event) {
+//   copyTextToClipboard('Jane');
+// });
---
+++ xkp/scripts/main.js
@@ -0,0 +1,101 @@
+
+init();
+
+function init() {
+
+  document.querySelector('#xkp-form')
+    .addEventListener('submit', function(ev) {
+      ev.preventDefault();
+      refresh();
+    }, false);
+  document.body.addEventListener('click', function(ev) {
+    if ( !ev.target.classList.contains('xkp-pass') ) return;
+    copyTextToClipboard(ev.target.textContent);
+  }, false);
+  refresh();
+}
+
+function refresh() {
+  var options = getFormValues();
+  var html = '';
+  var password;
+  for ( var i=0; i<4; i++ ) {
+    var password = xkpasswd.generate(options);
+    html += '<li class="xkp-item"><div class="xkp-pass">' + password + '</div></li>';
+  };
+  document.querySelector('.xkp-list').innerHTML = html;
+}
+
+function getFormValues() {
+  var values = {};
+  var possibles = [
+    'separators[]',
+    'numberOfWords',
+    'minWordLength',
+    'maxWordLength',
+    'capitalisationStyle',
+    'capitalisationStyle',
+    'prefixDigits',
+    'suffixDigits',
+    'punctuationPadding'
+  ];
+  possibles.forEach(function(name) {
+    var value = getFormValue(name);
+    if ( value !== undefined ) {
+      values[name] = value;
+    }
+  });
+  return values;
+}
+
+function getFormValue(name) {
+  var element, value;
+  var elements = document.querySelectorAll('*[name="'+name+'"]');
+  if ( elements.length === 1 ) {
+    element = elements[0];
+    switch(element.nodeName.toLowerCase()) {
+      case 'select':
+        value = element.options[element.selectedIndex].value;
+        break;
+      default:
+        switch (element.type) {
+          case 'number':
+            value = parseInt(element.value, 10);
+            break;
+          default:
+            value = element.value;
+        }
+    }
+  } else {
+    value = [];
+    [].slice.call(elements).forEach(function(element) {
+      if ( element.checked ) value.push(element.value);
+    });
+  }
+  return value;
+}
+
+function setFormValues(values) {
+  Object.keys(values).forEach(function(key) {
+    setFormValue(key, values[key]);
+  });
+}
+
+function setFormValue(name, value) {
+  var element, value, index;
+  var elements = document.querySelectorAll('*[name="'+name+'"]');
+  if ( elements.length === 1 ) {
+    element = elements[0];
+    switch(element.nodeName.toLowerCase()) {
+      case 'select':
+        element.options.forEach(function(option, index) {
+          if ( option.value === value ) {
+            element.selectedIndex = index;
+          }
+        });
+        break;
+      default:
+          element.value = value;
+    }
+  }
+}
